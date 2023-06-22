4a066b2ec165d08589bce7f1245617c24edf4191
Igor Basko <igorbasko@Igors-MacBook-Pro.local>
2023-06-22 20:19:23 +0300

    player informs that a move was made

--- Assets/Scripts/Player/HumanPlayerHandler.cs
+++ Assets/Scripts/Player/HumanPlayerHandler.cs
@@ -2,7 +2,11 @@ using System;
 public class HumanPlayerHandler {
     public event Action<int, int, PlayerMark> OnPerformMove;
     public HumanPlayerHandler(BoardUIEventsHandler boardUIEventsHandler) {
+        boardUIEventsHandler.OnCellClicked += OnCellClicked;
+    }
 
+    public void OnCellClicked(int x, int y) {
+        OnPerformMove?.Invoke(x, y, PlayerMark.X);
     }
 }
 
