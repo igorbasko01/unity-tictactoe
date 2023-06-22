5ea5e4b0a824849750026f5f3fdb22edec70bec8
igorbasko01 <igorbasko@gmail.com>
2023-06-22 20:58:20 +0300

    add BoardHandler (#4)
    Co-authored-by: Igor Basko <igorbasko@Igors-MacBook-Pro.local>

---
+++ Assets/Tests/Board/BoardHandlerTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class BoardHandlerTests {
+    [Test]
+    public void ReturnCellIsEmptyWhenCellIsEmpty() {
+        var boardHandler = new BoardHandler();
+        Assert.IsTrue(boardHandler.IsCellEmpty(0, 0));
+    }
+
+    [Test]
+    public void ReturnCellIsNotEmptyWhenCellIsOccupiedByPlayerX() {
+        var boardHandler = new BoardHandler();
+        boardHandler.PerformMove(0, 0, PlayerMark.X);
+        Assert.IsFalse(boardHandler.IsCellEmpty(0, 0));
+    }
+
+    [Test]
+    public void ReturnCellIsNotEmptyWhenCellIsOccupiedByPlayerO() {
+        var boardHandler = new BoardHandler();
+        boardHandler.PerformMove(0, 0, PlayerMark.O);
+        Assert.IsFalse(boardHandler.IsCellEmpty(0, 0));
+    }
+
+    [Test]
+    public void InvalidCoordinatesReturnCellIsNotEmpty() {
+        var boardHandler = new BoardHandler();
+        Assert.IsFalse(boardHandler.IsCellEmpty(-1, 0));
+        Assert.IsFalse(boardHandler.IsCellEmpty(0, -1));
+        Assert.IsFalse(boardHandler.IsCellEmpty(3, 0));
+        Assert.IsFalse(boardHandler.IsCellEmpty(0, 3));
+    }
+
+    [Test]
+    public void PerformMoveDoesntThrowAnExceptionIfCoordinatesAreInvalid() {
+        var boardHandler = new BoardHandler();
+        boardHandler.PerformMove(-1, 0, PlayerMark.X);
+        boardHandler.PerformMove(0, -1, PlayerMark.X);
+        boardHandler.PerformMove(3, 0, PlayerMark.X);
+        boardHandler.PerformMove(0, 3, PlayerMark.X);
+    }
+}
