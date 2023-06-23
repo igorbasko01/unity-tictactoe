e0c8b752e6b3681d70a07184b3ca07ab39a3d384
igorbasko01 <igorbasko@gmail.com>
2023-06-23 07:57:39 +0300

    perform the move when the cell is free (#8)
    Co-authored-by: Igor Basko <igorbasko@Igors-MacBook-Pro.local>

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,14 +1,20 @@
 using System;
 
 public class GameManager {
-    private readonly HumanPlayerHandler humanPlayerHandler;
+    private readonly HumanPlayerHandler _humanPlayerHandler;
+    private readonly BoardHandler _boardHandler;
     public event Action<int, int, PlayerMark> OnValidMove;
-    public GameManager(HumanPlayerHandler humanPlayerHandler) {
-        this.humanPlayerHandler = humanPlayerHandler;
-        humanPlayerHandler.OnPerformMove += OnPlayerPerformMove;
+    public GameManager(HumanPlayerHandler humanPlayerHandler, BoardHandler boardHandler) {
+        _humanPlayerHandler = humanPlayerHandler;
+        _boardHandler = boardHandler;
+        _humanPlayerHandler.OnPerformMove += OnPlayerPerformMove;
     }
 
     private void OnPlayerPerformMove(int x, int y, PlayerMark playerMark) {
+        if (!_boardHandler.IsCellEmpty(x, y)) {
+            return;
+        }
+        _boardHandler.PerformMove(x, y, playerMark);
         OnValidMove?.Invoke(x, y, playerMark);
     }
 }
